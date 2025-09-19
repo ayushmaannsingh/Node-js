2059c03eeb959e89efd58f69ac12ea136f839f8a
ayushmaannsingh <sa776857@gmail.com>
2025-09-20 03:17:29 +0530

    node js

---
+++ script.js
@@ -0,0 +1,26 @@
+// let n = 5;
+
+// for(let i=0; i<n; i++){
+//     console.log("Ayush,", i);
+// }
+
+// console.log("bye!");
+
+//console.log(process.argv);
+
+// let args = process.argv;
+
+// for(let i=2; i<args.length; i++){
+//     console.log("Hello to ", args[i]);
+// }
+
+
+// const math = require('./math ');
+
+// console.log(math.sum(5, 6));
+// console.log(math.pi);
+
+const info = require("./Fruits");
+
+console.log(info);
+
